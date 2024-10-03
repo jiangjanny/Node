51e45ead1d0248a38bcb9c61799d0ae52889a2dd
janny <yuaog1331@gmail.com>
2024-10-04 01:29:52 +0800

    建立 sql 與 mysql 連線

--- 09/app.js
+++ 09/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+const db = require('./util/database');
 
 const app = express();
 
@@ -13,6 +14,13 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+db.execute('SELECT * FROM products')
+    .then(([rows, fields]) => {
+        console.log('Products:', rows);  // 顯示查詢結果
+    })
+    .catch(err => {
+        console.error('Error executing query', err);
+    });
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
